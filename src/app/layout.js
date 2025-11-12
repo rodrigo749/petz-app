0b191a0a9df5dd6f9d4c290ab785931495bcb957
Kawan Mendes dos Santos <1051754@senacminas.edu.br>
2025-11-12 09:12:06 -0300

    feat:created bory

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,28 +20,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <header className="topbar">
-          <div className="container">
-            <div className="brand">
-              <div className="logo" aria-hidden="true"></div>
-              <div>
-                <div className="brand-title">Patas Perdidas</div>
-                <div className="brand-sub">Encontre • Adote • Ajude</div>
-              </div>
-            </div>
-
-            <nav className="toplinks" aria-label="menu">
-              <a href="#">Entrar</a>
-              <a href="#">Adoção</a>
-              <a href="#">Pets perdidos</a>
-              <a href="#">Apoiar</a>
-            </nav>
-          </div>
-        </header>
-
+        
         <main>{children}</main>
-
-       
       </body>
     </html>
   );
