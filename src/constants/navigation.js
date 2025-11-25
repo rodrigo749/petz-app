d94f69aab5e6c6e9d8cb1de10f1ec37ecf8256e5
Diego Henrique Vieira Ortiz <1065133@senacminas.edu.br>
2025-11-25 11:02:19 -0300

    fix bugs

--- src/constants/navigation.js
+++ src/constants/navigation.js
@@ -13,7 +13,7 @@ export const NAV_LINKS = [
     label: "Adoção ",
     href: "/adocao",
     subLinks: [
-      { label: "Pets para Adoção", href: "/adocao" },
+      { label: "Pets para Adoção", href: "/cadastro-pet-adocao" },
       { label: "Pets Adotados", href: "/adocao/adotados" },
     ],
   },
