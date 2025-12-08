4f1ae26489fdf8759700494c6c9cb1f649c1abb5
Angela Melina Silva Chaves <1062981@senacminas.edu.br>
2025-12-08 09:21:17 -0300

    Corrects image errors and updates pets.json; adds status for new pets

--- src/components/PetCard/PetCard.js
+++ src/components/PetCard/PetCard.js
@@ -12,20 +12,26 @@ export default function PetCard({ pet }) {
       <div className={styles["card-pet"]}>
         
         <div className={styles["card-image-wrapper"]}>
-          <img src={pet.imagem || "/images/default.png"} className={styles["card-image"]} />
+          <img
+            src={pet.imagem || "/images/default.png"}
+            alt={pet.nome}
+            className={styles["card-image"]}
+          />
         </div>
 
-        <div className={styles["card-text-box"]}>
-          <h3>{pet.nome}</h3>
-          <p>Raça: {pet.raca}</p>
-          <p>Gênero: {pet.genero}</p>
-          <p>Idade: {pet.idade}</p>
-          <p>Descrição: {pet.descricao}</p>
-        </div>
+        <div className={styles["content-column"]}>
+          <div className={styles["card-text-box"]}>
+            <h3>{pet.nome}</h3>
+            <p>Raça: {pet.raca}</p>
+            <p>Gênero: {pet.genero}</p>
+            <p>Idade: {pet.idade}</p>
+            <p>Descrição: {pet.descricao}</p>
+          </div>
 
-        <button className={styles["btn-adotar"]} onClick={() => setOpen(true)}>
-          Adotar
-        </button>
+          <button className={styles["btn-adotar"]} onClick={() => setOpen(true)}>
+            Adotar
+          </button>
+        </div>
 
       </div>
 
